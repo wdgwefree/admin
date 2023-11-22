836501ab334b6a8762e275070accfe8c2c1c94a5
wdg <1415064003@qq.com>
2023-11-22 17:46:14 +0800

    引入knife4j接口文档管理

--- src/main/java/com/wdg/demo/controller/MyController.java
+++ src/main/java/com/wdg/demo/controller/MyController.java
@@ -2,6 +2,8 @@ package com.wdg.demo.controller;
 
 import com.wdg.common.result.ApiResult;
 import com.wdg.demo.dto.ParamVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: wdg
  * @create: 2023-11-22 14:34
  */
+@Api(tags = "demo测试模块")
 @RequestMapping("/wdg")
 @RestController
 public class MyController {
 
+    @ApiOperation(value = "这个是测试test接口")
     @PostMapping("/test1")
     public ApiResult test1(@Validated @RequestBody ParamVO paramVO){
         return ApiResult.success();
