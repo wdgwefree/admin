9dab22265cfd26634cee43ba22e39979f60aa23d
wdg <1415064003@qq.com>
2024-04-22 16:37:47 +0800

    1

--- src/main/java/com/wdg/common/utils/GenerateCode.java
+++ src/main/java/com/wdg/common/utils/GenerateCode.java
@@ -13,19 +13,21 @@ public class GenerateCode {
 
     public static void main(String[] args) {
 
-        FastAutoGenerator.create("jdbc:mysql://81.70.190.102:50307/wdg?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8", "root", "TXroot123.")
+        String xmlPath = System.getProperty("user.dir")+"\\src\\main\\resources\\mapper\\system";
+        String outputDir=System.getProperty("user.dir")+"\\src\\main\\java";
+        FastAutoGenerator.create("jdbc:mysql://81.70.190.102:50307/admin?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8", "root", "TXroot123.")
                 .globalConfig(builder -> {
                     builder.author("wdg") // 设置作者
                             //.enableSwagger() // 开启 swagger 模式
-                            .outputDir("D://generator"); // 指定输出目录
+                            .outputDir(outputDir); // 指定输出目录
                 })
                 .packageConfig(builder -> {
                     builder.parent("com.wdg.system") // 设置父包名
                             //.moduleName("system") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D://generator")); // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, xmlPath)); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("sys_menu") // 设置需要生成的表名
+                    builder.addInclude("sys_user") // 设置需要生成的表名
                             //.addTablePrefix("app_", "c_") // 设置过滤表前缀
                             .entityBuilder()  //开启Entity 策略配置
                             .enableLombok() //开启 lombok
@@ -37,4 +39,5 @@ public class GenerateCode {
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
     }
+
 }
