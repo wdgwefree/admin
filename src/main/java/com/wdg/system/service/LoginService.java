f6044f6a46d5cf704a846457c0486b84146dbdc5
wdg <1415064003@qq.com>
2024-04-30 14:37:40 +0800

    1

--- src/main/java/com/wdg/system/service/LoginService.java
+++ src/main/java/com/wdg/system/service/LoginService.java
@@ -10,6 +10,7 @@ import com.wdg.system.entity.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+import org.springframework.util.StopWatch;
 
 import java.nio.charset.StandardCharsets;
 
@@ -20,6 +21,8 @@ public class LoginService {
     private ISysUserService iSysUserService;
 
     public String login(LoginDTO loginDTO) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("业务逻辑");
         String userAccount = loginDTO.getUserAccount();
         String password = loginDTO.getPassword();
         LambdaQueryWrapper<SysUser> queryWrapper = new LambdaQueryWrapper<SysUser>().eq(SysUser::getUserAccount, userAccount);
@@ -34,8 +37,13 @@ public class LoginService {
         if (!finalPwd.equals(sysUser.getPassword())){
             throw new BusinessException(ResultCode.USER_PASSWORD_ERROR);
         }
+        stopWatch.stop();
+        stopWatch.start("sa-token");
         StpUtil.login(userAccount);
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
         String tokenValue = StpUtil.getTokenInfo().getTokenValue();
         return tokenValue;
+
     }
 }
